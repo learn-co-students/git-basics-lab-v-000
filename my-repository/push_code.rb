9fbb93049b3eebb61928c77120a5e525e8dec82f
pmknyc <pmknyc@optimum.net>
2019-06-14 00:15:54 -0400

    input

--- my-repository/push_code.rb
+++ my-repository/push_code.rb
@@ -1,6 +1,9 @@
 def send_code
   system("cd ..")
   system("git add .")
+  puts "Enter a commit message"
+  input = gets.strip.to_s
+  system("git commit -m input")
   system("git push -u origin master")
 end
 
